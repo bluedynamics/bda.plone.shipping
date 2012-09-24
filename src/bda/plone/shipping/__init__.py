7e4efc2480b9b0f00df1366135c5f6c512ca56a2
Robert Niederreiter <office@squarewave.at>
2012-09-24 11:25:31 +0200

    i18n, FlatRate shipping

--- src/bda/plone/shipping/__init__.py
+++ src/bda/plone/shipping/__init__.py
@@ -1,1 +1,68 @@
-#
+from zope.interface import (
+    implementer,
+    Interface,
+)
+from zope.component import (
+    adapter,
+    getAdapter,
+    getAdapters,
+)
+from zope.i18nmessageid import MessageFactory
+from .interfaces import IShipping
+
+
+_ = MessageFactory('bda.plone.shipping')
+
+
+class Shippings(object):
+    
+    def __init__(self, context):
+        self.context = context
+    
+    def get(self, name):
+        return getAdapter(self.context, IShipping, name=name)
+    
+    @property
+    def shippings(self):
+        adapters = getAdapters((self.context,), IShipping)
+        return [_[1] for _ in adapters]
+    
+    @property
+    def vocab(self):
+        adapters = getAdapters((self.context,), IShipping)
+        return [(_[0], _[1].label) for _ in adapters if _[1].available]
+    
+    @property
+    def default(self):
+        adapters = getAdapters((self.context,), IShipping)
+        for name, shipping in adapters:
+            if shipping.default:
+                return name
+        if adapters:
+            return adapters[0][0]
+
+
+@implementer(IShipping)
+@adapter(Interface)
+class Shipping(object):
+    sid = None
+    label = None
+    available = False
+    default = False
+    
+    def __init__(self, context):
+        self.context = context
+    
+    def calculate(self, items):
+        raise NotImplementedError(u"Abstract ``Shipping`` does not implement "
+                                  u"``calculate``")
+
+
+class FlatRate(Shipping):
+    sid = 'flat_rate'
+    label = _('flat_rate', 'Flat Rate')
+    available = True
+    default = True
+    
+    def calculate(self, items):
+        pass
--- src/bda/plone/shipping/interfaces.py
+++ src/bda/plone/shipping/interfaces.py
@@ -1,6 +1,30 @@
-from zope.interface import Interface
+from zope.interface import (
+    Interface,
+    Attribute,
+)
 
 
 class IShippingExtensionLayer(Interface):
     """Browser layer for bda.plone.shipping.
     """
+
+
+class IShipping(Interface):
+    """Single shipping method.
+    """
+    sid = Attribute(u"Unique shipping method id. Shipping method adapter is "
+                    u"also registered under this name.")
+    
+    label = Attribute(u"Shipping method label")
+    
+    available = Attribute(u"Flag whether shipping method is available in "
+                          u"recent payment cycle.")
+    
+    default = Attribute(u"Flag whether this shipping method is default.")
+    
+    def calculate(self, items):
+        """Calculate shipping costs.
+        
+        @param items: items to calculate shipping costs.
+        @return: shipping costs as float.
+        """
