7465afec3b44761bb7f43f51cf299e34cc6beed7
Robert Niederreiter <office@squarewave.at>
2012-09-24 10:11:02 +0200

    stub

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0dev'
+shortdesc = "Shipping"
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setup(name='bda.plone.shipping',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+            'Environment :: Web Environment',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+      ],
+      author='BlueDynamics Alliance',
+      author_email='dev@bluedynamics.com',
+      license='GNU General Public Licence',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['bda', 'bda.plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
