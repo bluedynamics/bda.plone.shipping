d8c65ce026dbe4165f61f1506b1dd9ca4e87d3ef
Johannes Raggam <raggam-nl@adm.at>
2013-09-20 19:41:47 +0200

    test shipping mock

--- src/bda/plone/shipping/tests/test_shipping.py
+++ src/bda/plone/shipping/tests/test_shipping.py
@@ -3,6 +3,20 @@ from bda.plone.shipping.tests import (
     Shipping_INTEGRATION_TESTING,
     set_browserlayer,
 )
+from bda.plone.shipping import FlatRate, Shipping
+from zope.component import provideAdapter
+from zope.interface import Interface
+from bda.plone.shipping.interfaces import IShipping
+
+
+class MockShipping(Shipping):
+    sid = 'mock_shipping'
+    label = 'Mock Shipping'
+    available = True
+    default = False
+
+    def calculate(self, items):
+        return 10
 
 
 class TestShipping(unittest.TestCase):
@@ -12,6 +26,13 @@ class TestShipping(unittest.TestCase):
         self.portal = self.layer['portal']
         self.request = self.layer['request']
         set_browserlayer(self.request)
+        provideAdapter(MockShipping)
+        self.shipping = IShipping(self.portal)
 
-    def test_foo(self):
-        self.assertEquals(1, 1)
+    def test_shipping(self):
+        mock = self.shipping
+        self.assertEquals(mock.sid, "mock_shipping")
+        self.assertEquals(mock.label, "Mock Shipping")
+        self.assertEquals(mock.available, True)
+        self.assertEquals(mock.default, False)
+        self.assertEquals(mock.calculate([]), 10)
