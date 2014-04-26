61a1af4b20d0154f578106a3c784910ae381d89b
Robert Niederreiter <office@squarewave.at>
2014-04-26 18:07:47 +0200

    remove outdated import

--- src/bda/plone/shipping/tests/test_shipping.py
+++ src/bda/plone/shipping/tests/test_shipping.py
@@ -1,7 +1,6 @@
 import unittest2 as unittest
 from zope.component import provideAdapter
 from zope.interface import Interface
-from bda.plone.shipping import FlatRate
 from bda.plone.shipping import Shipping
 from bda.plone.shipping.tests import Shipping_INTEGRATION_TESTING
 from bda.plone.shipping.tests import set_browserlayer
