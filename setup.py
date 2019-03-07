5a363c6e965c7566fea8ab8611c1d0f4d6341775
Robert Niederreiter <office@squarewave.at>
2019-03-07 11:17:05 +0100

    License year. Cosmetics

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 
 version = '2.0.dev0'
-shortdesc = "Shipping"
+shortdesc = 'Shipping'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
@@ -16,11 +16,12 @@ setup(
     description=shortdesc,
     long_description=longdesc,
     classifiers=[
-        "Framework :: Plone",
-        "Framework :: Plone :: 5.1",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
+        'Framework :: Plone',
+        'Framework :: Plone :: 5.1',
+        'Framework :: Plone :: 5.2',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
     ],
     author='BlueDynamics Alliance',
     author_email='dev@bluedynamics.com',
@@ -43,4 +44,4 @@ setup(
     [z3c.autoinclude.plugin]
     target = plone
     """,
-    )
+)
